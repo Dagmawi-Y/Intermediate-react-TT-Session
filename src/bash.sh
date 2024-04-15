db519fd731265f8136503f9c6fc176fd7f237306
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2024-04-15 22:05:23 +0300

    script update

--- src/bash.sh
+++ src/bash.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Directory where your exercise files are located
+
 dir="exercises"
 
-# Loop over all JavaScript files in the directory
 for file in $dir/*.js
 do
-  # Get the base name of the file (without extension)
+
   base=$(basename "$file" .js)
 
-  # Create a new CSS file with the same base name
+
   touch "$dir/$base.css"
 
-  # Add an import statement at the top of the JavaScript file
+
   echo -e "import './$base.css';\n$(cat $file)" > $file
 done
